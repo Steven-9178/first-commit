04387f40b079e92b53d68e538b412da5325dc816
Steven-7788 <20895171@qq.com>
2025-10-17 01:01:11 +0800

    add hyx

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	engine := gin.Default()
 
-	engine.POST("/register", server.Register) /////cnm
+	engine.POST("/register", server.Register) /////cnm hyx
 
 	fmt.Println("我是hyx")
 	fmt.Println("我是sb")
