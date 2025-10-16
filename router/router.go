499dce54848025a1cdf290063dcc972529b68b99
Steven-7788 <20895171@qq.com>
2025-10-17 00:34:19 +0800

    修改日志

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"web/server"
 )
@@ -9,6 +10,8 @@ func main() {
 	engine := gin.Default()
 
 	engine.POST("/register", server.Register)
+
+	fmt.Println("我是hyx")
 
 	engine.Run(":8888")
 }
